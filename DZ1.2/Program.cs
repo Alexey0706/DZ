ac1040640c10eb982a938195c047f5a932df95d1
alexich0706 <alexich0706@rambler.ru>
2022-08-10 16:48:31 +0300

    DZ 1.2

--- DZ1.2/Program.cs
+++ DZ1.2/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write(value: "Введите первое число: ");
+int namber1 = Convert.ToInt32(value: Console.ReadLine());
+
+Console.Write(value: "Введите второе число: ");
+int namber2 = Convert.ToInt32(value: Console.ReadLine());
+
+int max = namber1;
+
+if(namber1 > max) max = namber1;
+if(namber2 > max) max = namber2;
+
+Console.WriteLine(value: $"MAX = {max}");
