6978dbd87d5470c62c0e3c508f736a17eeaf647d
alexich0706 <alexich0706@rambler.ru>
2022-08-10 19:54:39 +0300

    DZ 1.6

--- DZ1.6/Program.cs
+++ DZ1.6/Program.cs
@@ -1,1 +1,11 @@
 ﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Write(value: "Введите число: ");
+int number = Convert.ToInt32(value: Console.ReadLine());
+
+if(number % 2 == 0){
+    Console.WriteLine($"{number} is EVEN");
+}
+else
+{
+    Console.WriteLine($"{number} is ODD");
+}
