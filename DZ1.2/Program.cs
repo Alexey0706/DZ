ee979cecade7eb72202eaceb5b45a301086a3508
alexich0706 <alexich0706@rambler.ru>
2022-08-10 17:03:55 +0300

    DZ 1.2

--- DZ1.2/Program.cs
+++ DZ1.2/Program.cs
@@ -1,13 +1,18 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 Console.Write(value: "Введите первое число: ");
-int namber1 = Convert.ToInt32(value: Console.ReadLine());
+int number1 = Convert.ToInt32(value: Console.ReadLine());
 
 Console.Write(value: "Введите второе число: ");
-int namber2 = Convert.ToInt32(value: Console.ReadLine());
+int number2 = Convert.ToInt32(value: Console.ReadLine());
 
-int max = namber1;
+int max = number1;
 
-if(namber1 > max) max = namber1;
-if(namber2 > max) max = namber2;
-
-Console.WriteLine(value: $"MAX = {max}");
+if(number1 > number2){
+    Console.WriteLine($"{number1} is MAX");
+     }
+ if(number1 == number2){
+    Console.WriteLine($"{number1} is equal {number2}");
+     }
+if(number2 > number1){
+    Console.WriteLine($"{number2} is MAX");
+     }
